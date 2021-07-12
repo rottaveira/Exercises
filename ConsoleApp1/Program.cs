0c68595c5196a5a78d53aef66b3189663c3af22f
Rafael Taveira <rottaveira@gmail.com>
2021-07-12 13:28:23 -0300

    add exercicio

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            Example1.Run();
+            TwoSum.Run();
         }
    
     } 
--- ConsoleApp1/Public/TwoSum.cs
+++ ConsoleApp1/Public/TwoSum.cs
@@ -8,12 +8,59 @@ namespace Exercises
     {
         public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
         {
-            var conjuntosValidos = Enumerable.Range(0, 1 << (list.Count)).Select(index => list.Where((v, i) => (index & (1 << i)) != 0)).Where(w => w.Count() == 2 && w.Sum() == sum).ToArray();
-            var first = conjuntosValidos.FirstOrDefault();
 
-            if (first == null) return null;
+            /*Solução top, mas só passa 50%*/
+            /*var conjuntosValidos = Enumerable.Range(0, 1 << (list.Count)).Select(index => list.Where((v, i) => (index & (1 << i)) != 0)).Where(w => w.Count() == 2 && w.Sum() == sum).ToArray();
+             *  var first = conjuntosValidos.FirstOrDefault();
+             *
+             *  if (first == null) return null;
+             *
+             *  return new Tuple<int, int>(list.IndexOf(first.First()), list.IndexOf(first.Last()));
+             */
 
-            return new Tuple<int, int>(list.IndexOf(first.First()), list.IndexOf(first.Last()));
+
+            /*Segunda tentativa, passa só 50% tbm*/
+            /*for (int i = 0; i < list.Count; i++)
+             *  { 
+             *      var res = list.FirstOrDefault((a) => a + list[i] == sum);
+             *
+             *      if (res > 0)
+             *          return new Tuple<int, int>(list.IndexOf(res), list.IndexOf(list[i]));
+             *  }
+             *  return null;
+             */
+
+            /*terceira tentativa - passa só 50% tbm*/
+            /*  var count = list.Count;
+             *  var lista = list.ToList();
+             *  for (int i = 0; i < count; i++)
+             *  {
+             *
+             *      var res = lista.GetRange(i, count - (i + 1)).FirstOrDefault((a) => a + list[i] == sum);
+             *
+             *      if (res > 0)
+             *          return new Tuple<int, int>(list.IndexOf(res), list.IndexOf(list[i]));
+             *  }
+             *  return null;
+             */
+
+
+            /*terceira tentativa - passa só 50% tbm */
+            var teste = new List<Tuple<int, int>>();
+
+            int c = 0;
+            var dict = list.ToDictionary(v => c++, k => k);
+            for (int i = 0; i < list.Count; i++)
+             {
+                var valordesejado = sum - list[i];
+                if (dict.ContainsValue(valordesejado))
+                    return new Tuple<int, int>(i, dict.FirstOrDefault(f => f.Key != i && f.Value == valordesejado).Key); 
+                    //teste.Add( new Tuple<int, int>(i, dict.FirstOrDefault(f => f.Key != i && f.Value == valordesejado).Key)); 
+                    
+             }
+
+             return null;
+
         }
 
 
